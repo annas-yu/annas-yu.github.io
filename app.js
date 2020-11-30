c838891ef2ce34b5669bf85eb0b22568463a985b
kyle zhou <kyle.a.zhou@gmail.com>
2020-11-29 22:34:35 -0600

    title change, exploding modal, skills

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ function showSnackBar() {
         x.className = x.className.replace("show", "");
     }, 3000);
 }
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+
+function explode(imageClass) {
+    console.log(imageClass)
+    var modal = document.getElementById("myModal");
+    var img = document.getElementsByClassName(imageClass)[0];
+    var modalImg = document.getElementById("exploded-image");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+function closeExplode() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
